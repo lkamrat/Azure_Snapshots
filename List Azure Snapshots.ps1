69e95cfb180bc4b3e717387181a7aae911cf62f5
lkamrat <kamrat9@gmail.com>
2017-10-12 19:02:54 +0300

    Add script

---
+++ List Azure Snapshots.ps1
@@ -0,0 +1,2 @@
+﻿# List all Azure VM Snapshots in a Resource Group and output to a table
+Get-AzureRmSnapshot | Format-Table -Property Name,@{Name="Resource Group"; Expression={$_.ResourceGroupName}; Alignment="center"; },@{Name="Size in GB"; Expression={$_.DiskSizeGB}; Alignment="center"; } -AutoSize -Wrap
